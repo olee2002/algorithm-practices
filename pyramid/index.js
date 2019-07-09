57cf382852d881befc1cccefcd3be249bac7d199
babylee2002 <babylee2002@gmail.com>
2019-07-09 17:30:48 -0400

    pyramid iterative solution

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,20 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+   m = n*2-1
+   let midpt = Math.floor(m/2)
+   for(let i=0; i<n; i++){
+      let print = ''
+      for(let j=0; j<m; j++){
+        if(midpt-i<=j && midpt+i>=j){
+          print += '#'
+        } else {
+          print += ' '
+        }
+      } 
+     console.log(print)
+   }   
+}
 
 module.exports = pyramid;
