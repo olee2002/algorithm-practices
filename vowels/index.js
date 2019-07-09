891a06160df7c4e99e3366a314a303708bfe9e6c
babylee2002 <babylee2002@gmail.com>
2019-07-09 19:39:35 -0400

    vowels

--- vowels/index.js
+++ vowels/index.js
@@ -7,13 +7,28 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+// function vowels(str) {
+//    let vowels = "aeiou"
+//    let count = 0
+//       for(let i of str.toLowerCase()){
+//          if(vowels.includes(i)) count ++
+//       }
+   
+// return count
+// }
+module.exports = vowels;
+
 function vowels(str) {
+
    let vowels = "aeiou"
    let count = 0
       for(let i of str.toLowerCase()){
-         if(vowels.includes(i)) count ++
+         for(let j of vowels){
+           if(i===j){
+             count++
+           }
+         }
       }
    
 return count
 }
-module.exports = vowels;
