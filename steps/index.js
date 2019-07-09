aecb3dd8e3d5fec0476e247bb11eb4f3d42d369d
babylee2002 <babylee2002@gmail.com>
2019-07-09 16:04:52 -0400

    recursion method - need more practice

--- steps/index.js
+++ steps/index.js
@@ -17,18 +17,41 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-   for(let i=0; i<n; i++){
-      let print = ''
-      for(let j=0; j<n; j++){
-        if(i >=j ) {
-          print += '#' 
-        } else {
-          print += " "
-        }
-      }
-       console.log(print)
+//recursion method
+function steps(n, row=0, column=''){
+   if (n === row) {
+       return;
+     }
+     if (n === column.length) {
+        //print the result
+       console.log(column);
+       return steps(n, row + 1);
+     }
+   
+     if(column.length <=row){
+       column +='#';
+       // console.log(column)
+     } else {
+       column +=' ';
+     }
+     // const add = column.length <= row ? '#' : '@';
+     steps(n, row, column);
    }
-}
 
 module.exports = steps;
+
+
+//iteration
+// function steps(n) {
+//    for(let i=0; i<n; i++){
+//       let print = ''
+//       for(let j=0; j<n; j++){
+//         if(i >=j ) {
+//           print += '#' 
+//         } else {
+//           print += " "
+//         }
+//       }
+//        console.log(print)
+//    }
+// }
