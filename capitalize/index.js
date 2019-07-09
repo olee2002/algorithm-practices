262de318d92a4c96e1390a84838e11779aef49d9
babylee2002 <babylee2002@gmail.com>
2019-07-09 13:32:38 -0400

    capitalize

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,21 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const result = str[0].toUpperCase();
+  for(let i=0; i<str.length; i++){
+     if(i-1){
+        result+=str[i].toUpperCase();
+     } else {
+        result+=str[i]
+     }
+  }
+}
 
 module.exports = capitalize;
+
+
+// function capitalize(str) {
+//    const arr = str.split(' ')
+//    return arr.map(e=>e.slice(0,1).toUpperCase()+e.slice(1,e.length)).join(' ')
+// }
