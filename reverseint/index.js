69f1b4d9ef7ff659ce4cb778f8b20b7281c5baad
babylee2002 <babylee2002@gmail.com>
2019-07-09 10:02:08 -0400

    reverse int exercise

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+   if(n>=0){
+      return +n.toString().split('').reverse().join('')
+   } else {
+      let absN = Math.abs(n)
+      return -+absN.toString().split('').reverse().join('')
+   }
+}
 
 module.exports = reverseInt;
