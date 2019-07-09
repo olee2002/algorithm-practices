4cde469a63a1d9666e2ef37333ee86735f45ad8f
babylee2002 <babylee2002@gmail.com>
2019-07-09 09:47:06 -0400

    palindrome exercise

--- palindrome/index.js
+++ palindrome/index.js
@@ -7,6 +7,23 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+   let reversed = ''
+   for (let char of str){
+      reversed = char + reversed 
+   }
+   const arr = reversed.split('')
+   let isPanlindrome = false
+   for(let i=0; i<arr.length; i++){
+     console.log('test',arr[i],arr[arr.length-1-i] )
+      if(arr[i]===arr[arr.length-1-i]){
+         isPanlindrome = true
+      } else {
+        isPanlindrome = false
+        break;
+      }
+   }
+return isPanlindrome;
+}
 
 module.exports = palindrome;
