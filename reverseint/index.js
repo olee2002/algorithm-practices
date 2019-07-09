7886bc9a4fe194c4bb59df49b2b65e878f12cdb5
babylee2002 <babylee2002@gmail.com>
2019-07-09 10:20:28 -0400

    Math.sign usage to determine negative and positive numbers

--- reverseint/index.js
+++ reverseint/index.js
@@ -9,8 +9,8 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-
-      return +absN.toString().split('').reverse().join('') * Math.sign(n)
+   const reversed = n.toString().split('').reverse().join('')
+   return  parseInt(reversed) * Math.sign(n)
 }
 
 module.exports = reverseInt;
