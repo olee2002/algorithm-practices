f21af63865257b632747ecab912f5d55717dd890
babylee2002 <babylee2002@gmail.com>
2019-07-13 09:26:43 -0400

    js event class logic

--- bst/index.js
+++ bst/index.js
@@ -11,6 +11,31 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+   constructor(data){
+      this.data = data
+      this.left = null
+      this.right = null
+   }
+
+   insert(data) {
+      const dataPlace = data > this.data ? 'right' : 'left';
+      const dataPlaceValue = this[dataPlace];
+   
+      if (dataPlaceValue === null) this[dataPlace] = new Node(data);
+      else dataPlaceValue.insert(data);
+    }
+   // insert(data){
+   //    if(data < this.data && this.left){
+   //       this.left.insert(data)
+   //    } else if (data< this.data){
+   //       this.left = new Node(data)
+   //    } else if (data> this.data && this.right){
+   //       this.right.insert(data)
+   //    } else if(data> this.data){
+   //       this.right = new Node(data)
+   //    }
+   // }
+}
 
 module.exports = Node;
